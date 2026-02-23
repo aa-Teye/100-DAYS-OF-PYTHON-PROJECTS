397bd829cd20e329ef878e98f19131224454a39f
aa-Teye <alexteyeametepey@gmail.com>
2026-02-23 23:51:29 +0000

    Second Commit

--- DAY1_CONTACT_BOOK.py
+++ DAY1_CONTACT_BOOK.py
@@ -1,3 +1,8 @@
 # 100 DAYS OF PYTHON PROJECT1: DAY 1:CONTACT BOOK USING DICTIONARIES
+my_dict = { 
+    "name": "Lexis",
+    "phone": "123-456-7890",
+    "email": "lexis123@gmail.com"
+}
 
-
+print(my_dict.get("email"))
