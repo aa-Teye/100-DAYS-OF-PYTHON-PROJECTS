fcd1a8bfe6f261a094968e7a7878adf1874e59c2
aa-Teye <alexteyeametepey@gmail.com>
2026-02-23 23:57:35 +0000

    First Commit

--- DAY1_CONTACT_BOOK.py
+++ DAY1_CONTACT_BOOK.py
@@ -2,10 +2,14 @@
 my_dict = { 
     "name": "Lexis",
     "phone": "123-456-7890",
-    "email": "lexis123@gmail.com"
+    "email": "lexis123@gmail.com",
+    "house_address": "Mukose abutali street"
 }
 
 print(my_dict.get("email"))
 print(my_dict["name"])
 print(my_dict["phone"])
 print(my_dict.get("address"))
+print(my_dict.get("pho"))
+print(my_dict["house_address"])
+print(my_dict.get("house_address"))
