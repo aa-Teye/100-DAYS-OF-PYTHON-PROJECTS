ac403a62ab4fd21cdb9acb9e05346bc54590234c
aa-Teye <alexteyeametepey@gmail.com>
2026-02-24 00:42:02 +0000

    sixth commit

--- DAY1_CONTACT_BOOK.py
+++ DAY1_CONTACT_BOOK.py
@@ -3,15 +3,31 @@ my_dict = {
     "name": "Lexis",
     "phone": "123-456-7890",
     "email": "lexis123@gmail.com",
-    "house_address": "Mukose abutali street"
-    "Level of eduction": "Degree"
-}
+    "house_address": "Mukose abutali street",    
+    "Level of education": "Degree",
+    }
 
-print(my_dict.get("email"))
-print(my_dict["name"])
-print(my_dict["phone"])
-print(my_dict.get("address"))
-print(my_dict.get("pho"))
-print(my_dict["house_address"])
-print(my_dict.get("house_address"))
-print(my_dict.get("Level of eduction")) 
+#printing contents of the dictionary 
+
+# print(my_dict.get("email"))
+# print(my_dict["name"])
+# print(my_dict["phone"])
+# print(my_dict.get("address"))
+# print(my_dict.get("pho"))
+# print(my_dict["house_address"])
+# print(my_dict.get("house_address"))
+# print(my_dict.get("Level of education")) 
+
+
+# Example 2 
+Contact = { 
+    "first_name" : "Kelvin Labtech",
+    "phone_number" : "020856861",
+    "location": "Accra"
+}
+#Appending a new key value pair to the dictionary 
+Contact["email"] = "kelvinlabtech@gmail.com"
+Contact["email"] = "kelvinlabtech1234@gmail.com"
+del Contact["email"]
+del Contact["email"]
+print(Contact["email"])
